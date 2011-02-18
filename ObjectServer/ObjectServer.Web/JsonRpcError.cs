08a528b534522d2c79c382070dacca2cd26ce907
Wei Li <oldrev@gmail.com>
2011-02-18 22:40:25 +0800

    add file

---
+++ ObjectServer/ObjectServer.Web/JsonRpcError.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Newtonsoft.Json;
+
+namespace ObjectServer.Web
+{
+
+    [Serializable]
+    [JsonObject]
+    public sealed class JsonRpcError
+    {
+        [JsonProperty("errorCode", Required = Required.Always)]
+        public int ErrorCode { get; set; }
+
+        [JsonProperty("message", Required = Required.Always)]
+        public string Message { get; set; }
+    }
+}
