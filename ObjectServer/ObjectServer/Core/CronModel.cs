8a0efe2ea5c0706f3afb93defa796f81a6bab9fe
Wei Li <oldrev@gmail.com>
2011-02-19 01:08:28 +0800

    goto sleep

---
+++ ObjectServer/ObjectServer/Core/CronModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using ObjectServer.Model;
+
+namespace ObjectServer.Core
+{
+    [ServiceObject] 
+    public class CronModel : ModelBase
+    {
+
+        public CronModel()
+            : base()
+        {
+            this.Name = "core.cron";            
+        }
+
+
+    }
+}
