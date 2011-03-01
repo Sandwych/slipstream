ef58fdc949c3abacb3b1282fc6bcac9ae15c5f4e
Wei "oldrev"  Li <oldrev@gmail.com>
2011-03-01 16:04:33 +0800

    添加对 limit 和 offset 的测试

--- ObjectServer/ObjectServer.Test/Model/SearchTests.cs
+++ ObjectServer/ObjectServer.Test/Model/SearchTests.cs
@@ -17,13 +17,27 @@ namespace ObjectServer.Model.Test
         [Test]
         public void Test_search_limit()
         {
+            var domain = new object[][] { new object[] { "name", "like", "%" } };
             var ids = proxy.SearchModel(this.SessionId, "core.model",
-                new object[][] { new object[] {"name", "like", "%" } }, 0, 2);
+                domain, 0, 2);
             Assert.AreEqual(2, ids.Length);
 
             ids = proxy.SearchModel(this.SessionId, "core.model",
-                new object[][] { new object[] {"name", "like", "%" } }, 0, 3);
+                domain, 0, 3);
             Assert.AreEqual(3, ids.Length);
+        }
+
+        [Test]
+        public void Test_search_offset()
+        {
+            var domain = new object[][] { new object[] { "name", "like", "%" } };
+            var ids1 = proxy.SearchModel(this.SessionId, "core.model",
+                domain, 0, 2);
+            var ids2 = proxy.SearchModel(this.SessionId, "core.model",
+                domain, 1, 2);
+            Assert.AreNotEqual(ids1[0], ids2[0]);
+            Assert.AreEqual(ids1[1], ids2[0]);
+
         }
     }
 }
