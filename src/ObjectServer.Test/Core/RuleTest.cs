475098109537000afd5fbb9505c35749b6eddfef
Wei Li <oldrev@gmail.com>
2011-07-15 14:24:30 +0800

    添加rule 的单元测试的框架

---
+++ src/ObjectServer.Test/Core/RuleTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+namespace ObjectServer.Core.Test
+{
+
+    [TestFixture]
+    public sealed class RuleTest : LocalTestCase
+    {
+        [Ignore]
+        public void Test_ComputeDomain()
+        {
+
+        }
+
+    }
+}
