d8ba871bd08a11ec8f3ec03f000911a6aeee23eb
Wei "oldrev" Li <oldrev@gmail.com>
2011-08-27 22:05:09 +0800

    为模型改成 enum 的

--- src/ObjectServer.Core/Core/ModuleModel.cs
+++ src/ObjectServer.Core/Core/ModuleModel.cs
@@ -17,6 +17,15 @@ namespace ObjectServer.Core
     [Resource]
     public sealed class ModuleModel : AbstractTableModel
     {
+        public static class States
+        {
+            public const string Uninstalled = "uninstalled";
+            public const string Installed = "installed";
+            public const string ToInstall = "to-install";
+            public const string ToUpgrade = "to-upgrade";
+            public const string ToUninstall = "to-uninstall";
+        }
+
         public const string ModelName = "core.module";
         /// <summary>
         /// 单个数据库中加载的模块
@@ -28,7 +37,14 @@ namespace ObjectServer.Core
             this.AutoMigration = false;
 
             Fields.Chars("name").SetLabel("Name").Required().SetSize(128).Unique();
-            Fields.Chars("state").SetLabel("State").Required().SetSize(16);
+            Fields.Enumeration("state", new Dictionary<string, string>()
+            {
+                { States.Uninstalled, "Uninstalled" },
+                { States.Installed, "Installed" },
+                { States.ToInstall, "To Install" },
+                { States.ToUpgrade, "To Upgrade" },
+                { States.ToUninstall, "To Uninstall" },
+            }).Required().SetLabel("State");
             Fields.Text("info").SetLabel("Information");
         }
 
