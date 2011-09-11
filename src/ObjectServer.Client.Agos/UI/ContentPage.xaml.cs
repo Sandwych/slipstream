21849ed9fcf234d102ebaf2aa48c3a4d2ed5e75b
Wei "oldrev" Li <oldrev@gmail.com>
2011-09-11 14:24:15 +0800

    前台调整

---
+++ src/ObjectServer.Client.Agos/UI/ContentPage.xaml.cs
@@ -0,0 +1,148 @@
+﻿namespace ObjectServer.Client.Agos
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Navigation;
+
+    using ObjectServer.Client;
+    using ObjectServer.Client.Model;
+
+    /// <summary>
+    /// Home page for the application.
+    /// </summary>
+    public partial class ContentPage : Page
+    {
+        /// <summary>
+        /// Creates a new <see cref="MainPage"/> instance.
+        /// </summary>
+        public ContentPage()
+        {
+            InitializeComponent();
+            this.Menu.SelectedValuePath = "Id";
+        }
+
+        /// <summary>
+        /// Executes when the user navigates to this page.
+        /// </summary>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var app = (App)Application.Current;
+            this.Title = ApplicationStrings.HomePageTitle;
+            this.TextUserName.Text = app.ClientService.LoggedUserName;
+            this.TextServerUri.Text = app.ClientService.ServerAddress.ToString();
+            app.IsBusy = true;
+
+            app.ClientService.ReadAllMenus(menus =>
+            {
+                this.LoadMenus(menus);
+                app.IsBusy = false;
+            });
+        }
+
+        private void Menu_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            var app = App.Current as App;
+
+            var tvi = this.Menu.SelectedItem as TreeViewItem;
+            var menu = tvi.DataContext as MenuModel;
+
+            if (menu.Action != null)
+            {
+                this.OpenActionTab(menu);
+            }
+        }
+
+
+        private void OpenActionTab(MenuModel menu)
+        {
+            //先看看有没有已经打开同样的动作标签页了，如果有就跳转过去
+            var actWin = new Windows.ListWindow();
+
+            var actionName = menu.Action.Item1;
+
+            var tabPage = new TabItem();
+            tabPage.DataContext = menu.Action;
+            tabPage.Content = actWin;
+            tabPage.Header = actionName;
+            this.TabContainer.Items.Add(tabPage);
+            this.TabContainer.SelectedItem = tabPage;
+
+            actWin.Load(menu.Action.Item2);
+        }
+
+
+        public void LoadMenus(IEnumerable<MenuModel> menus)
+        {
+            this.Menu.Items.Clear();
+            this.InsertMenus(menus);
+        }
+
+        private void InsertMenus(IEnumerable<MenuModel> menus)
+        {
+            var rootMenus =
+                from m in menus
+                where m.ParentId == null
+                orderby m.Ordinal
+                select m;
+
+            foreach (var menu in rootMenus)
+            {
+                var node = InsertMenu(null, menu);
+
+                InsertSubmenus(menus, menu, node);
+            }
+        }
+
+
+        private TreeViewItem InsertMenu(TreeViewItem parent, MenuModel menu)
+        {
+            var node = new TreeViewItem();
+            node.Header = menu.Name;
+            node.DataContext = menu;
+            node.DisplayMemberPath = "Name";
+
+            if (parent != null)
+            {
+                parent.Items.Add(node);
+            }
+            else
+            {
+                this.Menu.Items.Add(node);
+            }
+            return node;
+        }
+
+        private void InsertSubmenus(
+            IEnumerable<MenuModel> menus, MenuModel parentMenu, TreeViewItem parentNode)
+        {
+            //子菜单们
+            var submenus =
+                from m in menus
+                where m.ParentId != null && m.ParentId == parentMenu.Id
+                orderby m.Ordinal
+                select m;
+
+            foreach (var menu in submenus)
+            {
+                var node = InsertMenu(parentNode, menu);
+                //再把子子菜单们找出来
+                InsertSubmenus(menus, menu, node);
+            }
+        }
+
+
+        private void LogoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            var app = (App)Application.Current;
+            app.ClientService.LogOff(
+                () =>
+                {
+                    app.PrepareToLogin();
+                });
+        }
+
+    }
+}
---
+++ src/ObjectServer.Client.Agos/UI/LoginPage.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+using System.Collections;
+using System.Threading;
+
+using ObjectServer.Client.Agos.Controls;
+using ObjectServer.Client.Agos.Models;
+
+namespace ObjectServer.Client.Agos.Views
+{
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+
+            this.textServer.Text = @"http://localhost:9287";
+            this.textLogin.Text = "root";
+            this.textPassword.Password = "root";
+            this.LoadDatabaseList();
+
+            this.Loaded += new RoutedEventHandler(LoginPage_Loaded);
+        }
+
+
+        void LoginPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = new LoginModel()
+            {
+                Login = "root",
+                Password = "root",
+            };
+        }
+
+        private void LoadDatabaseList()
+        {
+            var client = new ObjectServerClient(new System.Uri(this.textServer.Text));
+
+            var app = (App)Application.Current;
+            app.IsBusy = true;
+            client.ListDatabases(dbs =>
+            {
+                this.listDatabases.ItemsSource = dbs;
+
+                if (dbs.Length >= 1)
+                {
+                    this.listDatabases.SelectedIndex = 0;
+                }
+
+                app.IsBusy = false;
+            });
+        }
+
+        // Executes when the user navigates to this page.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+
+        }
+
+        private void buttonChangeServer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            Signin();
+        }
+
+        private void Signin()
+        {
+
+            var database = (string)this.listDatabases.SelectedValue;
+            var login = this.textLogin.Text.Trim();
+            var password = this.textPassword.Password;
+
+            var app = (App)Application.Current;
+            app.IsBusy = true;
+
+            var client = new ObjectServerClient(new Uri(this.textServer.Text));
+
+            client.LogOn(database, login, password,
+                sid =>
+                {
+                    if (string.IsNullOrEmpty(sid))
+                    {
+                        this.textMessage.Text = "登录失败，请检查用户名与密码";
+                    }
+                    else
+                    {
+                        app.ClientService = client;
+                        app.MainPage.NavigateToContentPage();
+                    }
+
+                    app.IsBusy = false;
+                });
+
+        }
+
+        private void Page_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter && this.buttonSignIn.IsEnabled)
+            {
+                this.buttonSignIn_Click(sender, e);
+            }
+        }
+
+        private void ButtonDatabaseManagement_Click(object sender, RoutedEventArgs e)
+        {
+            var app = (App)Application.Current;
+            app.MainPage.NavigateToDatabasesPage();
+        }
+
+    }
+}
