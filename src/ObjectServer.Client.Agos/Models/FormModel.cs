bf7310e49524c6d46b7202bb44ed83d9db913842
Wei Li <oldrev@gmail.com>
2011-09-29 14:09:14 +0800

    完善绑定

--- src/ObjectServer.Client.Agos/Models/FormModel.cs
+++ src/ObjectServer.Client.Agos/Models/FormModel.cs
@@ -12,8 +12,8 @@ namespace ObjectServer.Client.Agos.Models
         private readonly Dictionary<string, object> record =
             new Dictionary<string, object>();
 
-        private readonly Dictionary<string, List<object>> errors =
-            new Dictionary<string, List<object>>();
+        private readonly Dictionary<string, List<string>> errors =
+            new Dictionary<string, List<string>>();
 
         public FormModel()
         {
@@ -36,7 +36,6 @@ namespace ObjectServer.Client.Agos.Models
                 this.PropertyChanged(this, new PropertyChangedEventArgs(property));
             }
         }
-
 
         #region INotifyDataErrorInfo Members
 
@@ -86,10 +85,63 @@ namespace ObjectServer.Client.Agos.Models
                 return false;
             }
 
+            //TODO 验证字段
+            //绑定语法 Text="{Binding [Title], Mode=TwoWay}"
+
+
             this.record[binder.Name] = value;
             this.NotifyPropertyChanged(binder.Name);
 
             return base.TrySetMember(binder, value);
         }
+
+        // Adds the specified error to the errors collection if it is not 
+        // already present, inserting it in the first position if isWarning is 
+        // false. Raises the ErrorsChanged event if the collection changes. 
+        public void AddError(string propertyName, string error, bool isWarning)
+        {
+            if (!errors.ContainsKey(propertyName))
+            {
+                errors[propertyName] = new List<string>();
+            }
+
+            if (!errors[propertyName].Contains(error))
+            {
+                if (isWarning)
+                {
+                    errors[propertyName].Add(error);
+                }
+                else
+                {
+                    errors[propertyName].Insert(0, error);
+                }
+                RaiseErrorsChanged(propertyName);
+            }
+        }
+
+        // Removes the specified error from the errors collection if it is
+        // present. Raises the ErrorsChanged event if the collection changes.
+        public void RemoveError(string propertyName, string error)
+        {
+            if (errors.ContainsKey(propertyName) &&
+                errors[propertyName].Contains(error))
+            {
+                errors[propertyName].Remove(error);
+                if (errors[propertyName].Count == 0)
+                {
+                    errors.Remove(propertyName);
+                }
+                this.RaiseErrorsChanged(propertyName);
+            }
+        }
+
+        public void RaiseErrorsChanged(string propertyName)
+        {
+            if (this.ErrorsChanged != null)
+            {
+                this.ErrorsChanged(this, new DataErrorsChangedEventArgs(propertyName));
+            }
+        }
+
     }
 }
