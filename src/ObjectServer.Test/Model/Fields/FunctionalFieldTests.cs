cd856a4e5f9ad8876fc24f543bea64c586a0880e
Wei "oldrev" Li <oldrev@gmail.com>
2011-10-05 14:55:25 +0800

    又处理一个单元测试

--- src/ObjectServer.Test/Model/Fields/FunctionalFieldTests.cs
+++ src/ObjectServer.Test/Model/Fields/FunctionalFieldTests.cs
@@ -18,20 +18,19 @@ namespace ObjectServer.Model.Fields.Test
         private dynamic PrepareTestData()
         {
             dynamic data = new ExpandoObject();
+            dynamic model = this.GetResource(ModelName);
 
             data.record1 = new ExpandoObject();
             data.record1.name = "test1";
             data.record1.field1 = 1;
             data.record1.field2 = 2;
-            data.record1_id = this.Service.Execute(
-                TestingDatabaseName, this.SessionId, ModelName, "Create", data.record1);
+            data.record1_id = model.Create(this.TransactionContext, data.record1);
 
             data.record2 = new ExpandoObject();
             data.record2.name = "test2";
             data.record2.field1 = 5;
             data.record2.field2 = 4;
-            data.record2_id = this.Service.Execute(
-                TestingDatabaseName, this.SessionId, ModelName, "Create", data.record2);
+            data.record2_id = model.Create(this.TransactionContext, data.record2);
 
             return data;
         }
@@ -39,39 +38,37 @@ namespace ObjectServer.Model.Fields.Test
         [SetUp]
         public void ClearTestData()
         {
-            long[] ids = (long[])this.Service.Execute(
-                TestingDatabaseName, this.SessionId, ModelName, "Search", null, null, 0, 0);
+            dynamic model = this.GetResource(ModelName);
+            long[] ids = (long[])model.Search(this.TransactionContext, null, null, 0, 0);
             var idsToDel = ids.Select(o => (object)o).ToArray();
-            this.Service.Execute(
-                TestingDatabaseName, this.SessionId, ModelName, "Delete", new object[] { idsToDel });
+            model.Delete(this.TransactionContext, idsToDel);
         }
 
         [Test]
-        public void Test_function_many2one()
+        public void CanUseFunctionalManyToOneField()
         {
             var constraints = new object[][] { new object[] { "login", "=", "root" } };
-            dynamic ids = this.Service.Execute(
-                TestingDatabaseName, this.SessionId, "core.user", "Search", constraints, null, 0, 0);
+            dynamic userModel = this.GetResource("core.user");
+            dynamic ids = userModel.Search(this.TransactionContext, constraints, null, 0, 0);
 
             dynamic data = PrepareTestData();
 
-            dynamic records = this.Service.Execute(
-                TestingDatabaseName, this.SessionId, ModelName, "Read", new object[] { data.record1_id }, null);
+            dynamic records = userModel.Read(
+                this.TransactionContext, new object[] { data.record1_id }, null, 0, 0);
 
             var userField0 = (object[])records[0]["user"];
             Assert.AreEqual(ids[0], userField0[0]);
         }
 
         [Test]
-        public void Test_function_field_as_constraint()
+        public void CanFunctionFieldAsConstraint()
         {
+            dynamic model = this.GetResource(ModelName);
             var constraints = new object[][] { new object[] { "sum_field", "=", 9 } };
             dynamic data = PrepareTestData();
 
             var args = new object[] { constraints };
-            dynamic n = this.Service.Execute(
-                TestingDatabaseName, this.SessionId, ModelName, "Count", args);
-
+            dynamic n = model.Count(this.TransactionContext, args);
             Assert.AreEqual(1, n);
         }
     }
