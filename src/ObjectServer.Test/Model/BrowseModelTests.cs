fb0e4b706cb1e5d58764e67b98b4d1a17b43112c
Wei "oldrev" Li <oldrev@gmail.com>
2011-10-12 00:04:51 +0800

    单元测试调整

--- src/ObjectServer.Test/Model/BrowseModelTests.cs
+++ src/ObjectServer.Test/Model/BrowseModelTests.cs
@@ -37,35 +37,5 @@ namespace ObjectServer.Model.Test
             Assert.AreEqual("child-obj", dynamicMaster.children[0].name);
         }
 
-        [Test]
-        public void CanBrowseReferenceField()
-        {
-            this.ClearTestModelTable();
-            this.ClearMasterAndChildTable();
-
-            var masterModel = this.GetResource("test.master");
-            var childModel = this.GetResource("test.child");
-            var testModel = this.GetResource("test.test_model");
-
-            //创建 Master 与 Child 测试数据
-            dynamic masterRecord1 = new ExpandoObject();
-            masterRecord1.name = "master1";
-            var masterId1 = masterModel.Create(this.TransactionContext, masterRecord1);
-
-            dynamic childRecord1 = new ExpandoObject();
-            childRecord1.name = "child1";
-            var childId1 = childModel.Create(this.TransactionContext, childRecord1);
-
-            //创建测试数据 TestModel
-            dynamic testRecord1 = new ExpandoObject();
-            testRecord1.name = "test1";
-            testRecord1.address = "address1";
-            testRecord1.reference_field = new object[] { "test.master", masterId1 };
-            var testId1 = testModel.Create(this.TransactionContext, testRecord1);
-
-            dynamic obj = testModel.Browse(this.TransactionContext, testId1);
-
-            Assert.AreEqual("master1", obj.reference_field.name);
-        }
     }
 }
