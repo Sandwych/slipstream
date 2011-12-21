7259266421ddd834890db0502b7951e29af27272
Wei "oldrev" Li <oldrev@gmail.com>
2011-12-22 00:24:07 +0800

    菜单添加图标

--- src/ObjectServer.Core/Core/MenuModel.cs
+++ src/ObjectServer.Core/Core/MenuModel.cs
@@ -21,6 +21,7 @@ namespace ObjectServer.Core
             Fields.Integer("ordinal").SetLabel("Ordinal Number")
                 .Required().SetDefaultValueGetter(arg => 0);
             Fields.ManyToOne("parent", "core.menu").SetLabel("Parent Menu").NotRequired();
+            Fields.Chars("icon").SetLabel("Icon Name").NotRequired();
             Fields.Boolean("active").SetLabel("Active").Required().SetDefaultValueGetter(arg => true);
             Fields.Reference("action").SetLabel("Action").NotRequired().SetOptions(
                    new Dictionary<string, string>()
